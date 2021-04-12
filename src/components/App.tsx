79348d358e6e4a3dd10eea43d41f3acd6fa4e956
Jordan Hudgens <jordan@devcamp.com>
2021-04-12 10:44:54 -0700

    02_react_router

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,30 @@
-import * as React from "react";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import Hello from "./Hello";
-import HelloWithHooks from "./HelloWithHooks";
+import About from "../pages/About";
+import Home, { Banner } from "../pages/Home";
 
 const App = () => {
   return (
-    <div>
-      <Hello compiler={"TS"} framework={"React"} />
-      <HelloWithHooks />
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+
+        <Link to="/about">About</Link>
+      </div>
+
+      <Banner />
+
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+
+        <Route path="/about" exact>
+          <About />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const Banner = () => {
+  return <div>Some banner stuff</div>;
+};
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <div>Have some content</div>
+    </div>
+  );
+};
+
+export default Home;
